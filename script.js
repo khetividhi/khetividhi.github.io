2ec1457f455843c4d6a664d6d170a790e018be2a
SaloniMathur128 <saloni.m2211@gmail.com>
2024-07-09 16:11:19 +0530

    aa

--- script.js
+++ script.js
@@ -11,4 +11,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
- 
+
+document.addEventListener("DOMContentLoaded", function() {
+    const animatedElement = document.querySelector('#inappimg','#detectimg ','#wetherimg', '#leaseimg',  '#pestimg', '#demandimg' );
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Trigger the animation
+          animatedElement.classList.add('animate');
+
+          // Remove the 'animate' class after the animation ends to allow replay
+          animatedElement.addEventListener('animationend', () => {
+            animatedElement.classList.remove('animate');
+          }, { once: true });
+        }
+      });
+    }, { threshold: 0.10 }); // Adjust threshold as needed
+
+    observer.observe(animatedElement);
+  });
